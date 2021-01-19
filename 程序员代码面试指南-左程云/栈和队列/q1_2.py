bade56a61c44889890aa087386bdbdd5db7b0ccd
yearing1017@126.com <yearing1017@126.com>
2021-01-19 09:40:35 +0800

    add q1&q2

---
+++ 程序员代码面试指南-左程云/栈和队列/q1_2.py
@@ -0,0 +1,42 @@
+"""
+问题描述：构造一个特殊的栈，要求
+1）使之pop()、push()和getMin() (求最小值) 的操作的时间复杂度都为O(1)
+2）可以使用现成的栈类型
+"""
+
+# 思路1：使用两个栈，一个用来保存当前栈的元素，功能与正常栈一样；另一个栈用于保存每一步的最小值；针对压入时规则设定
+
+class MyStack:
+    def __init__(self):
+        self.stackData = list()
+        self.stackMin = list()
+    def push(self, value):
+        length = len(self.stackMin)
+        if length == 0:
+            self.stackMin.append(value)
+        elif value <= self.getMin():
+            self.stackMin.append(value)
+        else:
+            self.stackMin.append(self.getMin())
+        self.stackData.append(value)
+    def pop(self):
+        if len(self.stackData) == 0:
+            print('stack is empty!')
+        self.stackMin.pop()
+        value = self.stackData.pop()
+        return value
+    def getMin(self):
+        if len(self.stackMin) == 0:
+            print('stack is empty!')
+        return self.stackMin[-1]
+
+if __name__ == '__main__':
+    stack = MyStack()
+    stack.push(3)
+    print(stack.getMin())
+    stack.push(4)
+    print(stack.getMin())
+    stack.push(1)
+    print(stack.getMin())
+    stack.pop()
+    print(stack.getMin())
---
+++ 程序员代码面试指南-左程云/栈和队列/q2.py
@@ -0,0 +1,44 @@
+"""
+问题描述：编写一个类，用两个栈实现一个队列，要求:
+支持队列的基本操作，add、poll和peek
+
+思路：使用两个栈模拟队列操作
+"""
+
+class MyQuene:
+    def __init__(self):
+        self.stack_in = list()
+        self.stack_out = list()
+
+    def pushToPop(self):
+        if len(self.stack_out) == 0:
+            while len(self.stack_in) > 0:
+                self.stack_out.append(self.stack_in.pop())
+
+    def add(self, value):
+        self.stack_in.append(value)
+        self.pushToPop()
+
+    def poll(self):
+        if len(self.stack_out) == 0 and len(self.stack_in) == 0:
+            print('quene is empty!')
+        self.pushToPop()
+        return self.stack_out.pop()
+    
+    def peek(self):
+        if len(self.stack_out) == 0 and len(self.stack_in) == 0:
+            print('quene is empty!')
+        self.pushToPop()
+        return self.stack_out[-1]
+
+if __name__ == '__main__':
+    queue = MyQuene()
+    queue.add(1)
+    queue.add(2)
+    queue.add(3)
+    print(queue.peek())
+    print(queue.poll())
+    print(queue.peek())
+    print(queue.poll())
+    print(queue.peek())
+    print(queue.poll())
