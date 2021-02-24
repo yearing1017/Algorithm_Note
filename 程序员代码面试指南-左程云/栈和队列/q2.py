10318674da1ae3150ece5fcf4723ec18ac8d29bf
yearing1017@126.com <yearing1017@126.com>
2021-02-24 21:53:43 +0800

    update q2

--- 程序员代码面试指南-左程云/栈和队列/q2.py
+++ 程序员代码面试指南-左程云/栈和队列/q2.py
@@ -31,6 +31,26 @@ class MyQuene:
         self.pushToPop()
         return self.stack_out[-1]
 
+'''
+# 剑指offer中的一个简单版本
+class CQueue:
+
+    def __init__(self):
+        self.stack_in = list()
+        self.stack_out = list()
+
+    def appendTail(self, value: int) -> None:
+        self.stack_in.append(value)
+
+    def deleteHead(self) -> int:
+        if self.stack_out: return self.stack_out.pop()
+        elif not self.stack_in: return -1
+        else:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+        return self.stack_out.pop()
+'''
+
 if __name__ == '__main__':
     queue = MyQuene()
     queue.add(1)
