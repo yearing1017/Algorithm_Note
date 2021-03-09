74c19a2ce6225d24516086c817e0ee89212b6d45
yearing1017@126.com <yearing1017@126.com>
2021-03-09 10:43:20 +0800

    add q22

---
+++ 程序员代码面试指南-左程云/递归和动态规划/q22.py
@@ -0,0 +1,74 @@
+'''
+对于一个字符串，请设计一个高效算法，计算其中最长回文子串的长度。
+给定字符串A以及它的长度n，请返回最长回文子串
+https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zhong-xin-kuo-san-dong-tai-gui-hua-by-liweiwei1419/
+
+'''
+
+# 动态规划法
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        size = len(s)
+        if size < 2:
+            return s
+
+        dp = [[False for _ in range(size)] for _ in range(size)]
+
+        max_len = 1
+        start = 0
+
+        for i in range(size):
+            dp[i][i] = True
+
+        for i in range(size-2, -1, -1):
+            for j in range(i+1, size):
+                if s[i] == s[j]:
+                    if j - i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = False
+
+                if dp[i][j]:
+                    cur_len = j - i + 1
+                    if cur_len > max_len:
+                        max_len = cur_len
+                        start = i
+        return s[start:start + max_len]
+
+# 中心扩散
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        size = len(s)
+        if size < 2:
+            return s
+
+        # 至少是 1
+        max_len = 1
+        res = s[0]
+
+        for i in range(size):
+            palindrome_odd, odd_len = self.__center_spread(s, size, i, i)
+            palindrome_even, even_len = self.__center_spread(s, size, i, i + 1)
+
+            # 当前找到的最长回文子串
+            cur_max_sub = palindrome_odd if odd_len >= even_len else palindrome_even
+            if len(cur_max_sub) > max_len:
+                max_len = len(cur_max_sub)
+                res = cur_max_sub
+
+        return res
+
+    def __center_spread(self, s, size, left, right):
+        """
+        left = right 的时候，此时回文中心是一个字符，回文串的长度是奇数
+        right = left + 1 的时候，此时回文中心是一个空隙，回文串的长度是偶数
+        """
+        i = left
+        j = right
+
+        while i >= 0 and j < size and s[i] == s[j]:
+            i -= 1
+            j += 1
+        return s[i + 1:j], j - i - 1
