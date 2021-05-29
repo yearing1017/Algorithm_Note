a652f182234d91034e57fd2377970d3095e2ab0f
yearing1017@126.com <yearing1017@126.com>
2021-05-29 21:36:47 +0800

    修改

--- 程序员代码面试指南-左程云/字符串问题/q14.py
+++ 程序员代码面试指南-左程云/字符串问题/q14.py
@@ -1,13 +1,13 @@
 """
 问题描述：给定字符串str，判断是不是整体有效的括号字符串。
 举例：
-str="()"，返回true;str="(()())",返回true;str="(())"，返回true.
-str="(()"，返回false;str="()(",返回false;str="()a()"，返回false.
+str="()"，返回true;str="(()())",返回true;str="(())"， 返回true.
+str="(()"，返回false;str="()(",返回false;str="()a()"， 返回false.
 
 补充题目：
 给定一个括号字符串str，返回最长的有效括号子串。
 举例：
-str="(()())",返回6；str="())"，返回2;str="()(()()(",返回4。
+str="(()())",返回6；str="())"，返回2;str="()(()()(",  返回4。
 """
 
 class ParenthesesProblem:
