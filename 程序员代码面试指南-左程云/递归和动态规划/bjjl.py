dc7545b3af4fd3b888b4f6c876bcf9752e9fb026
yearing1017@126.com <yearing1017@126.com>
2021-08-15 16:57:44 +0800

    update

--- 程序员代码面试指南-左程云/递归和动态规划/bjjl.py
+++ 程序员代码面试指南-左程云/递归和动态规划/bjjl.py
@@ -24,20 +24,24 @@ class Solution:
         # dp[i][j] 表示word1[1...i]变为word2[1...j]的编辑距离
         dp = [[0 for _ in range(col+1)]for _ in range(row+1)]
 
-        # 第一行：word1由空字符串变为word2的步数
+        # 第一行：word1由空字符串变为word2的步数  每次都新增一个字符
         for j in range(1, col+1):
             dp[0][j] = dp[0][j-1] + 1
         
-        # 第一列：word1变为word2空字符串的步数
+        # 第一列：word1由原字符串变为word2空字符串的步数 每次都删除一个字符  
         for i in range(1, row+1):
             dp[i][0] = dp[i-1][0] + 1
 
         for i in range(1, row+1):
             for j in range(1, col+1):
-                # 当尾部字符相等时
+                # 当尾部字符相等时 等价于去看前面的字符转换所需步数
                 if word1[i-1] == word2[j-1]:
                     dp[i][j] = dp[i-1][j-1]
                 else:
+                    # 三种情况取最小 + 1
+                    # 情况1：word1:xxxxE word2:xxxxF 替换尾部字符 说明等于前面字符的转换次数 + word1的1次替换次数
+                    # 情况2：word1:xxxD  word2:xxx   删除word1的尾部字符  说明等于前面字符的转换次数 + word1的1次删除次数
+                    # 情况3：word1:xxx  word2:xxxD   word1尾部添加字符   说明等于前面字符的转换次数 + word1的1次添加次数
                     dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
         return dp[-1][-1]
 
--- 程序员代码面试指南-左程云/递归和动态规划/q13.py
+++ 程序员代码面试指南-左程云/递归和动态规划/q13.py
@@ -1,6 +1,5 @@
 """
-问题描述：给定两个字符串str1和str2，再给定三个整数ic、dc和rc，分别代表插入、删除
-和替换一个字符的代价，返回将str1编辑成str2的最小代价。
+问题描述：给定两个字符串str1和str2，再给定三个整数ic、dc和rc，分别代表插入、删除和替换一个字符的代价，返回将str1编辑成str2的最小代价。
 
 举例：
 str1='abc', str2='adc', ic=5, dc=3, rc=2
@@ -23,22 +22,24 @@ class LowestEditCost:
         len2 = len(str2)
         # dp[i][j] 表示将str1[0...i-1] 编辑成 str2[0..j-1]的最小代价
         dp = [[0 for _ in range(len2+1)] for _ in range(len1+1)]
-        # 第一列
+        # 第一列  word1由原字符串变为word2空字符串的步数 每次都删除一个字符  
         for i in range(len1+1):
             dp[i][0] = dc * i
-        # 第一行
+        # 第一行  word1由空字符串变为word2的步数  每次都新增一个字符
         for j in range(len2+1):
             dp[0][j] = ic * j
 
         for i in range(1, len1+1):
             for j in range(1, len2+1):
+                # 当尾部字符相等时 等价于去看前面的字符转换所需步数
                 if str1[i-1] == str2[j-1]:
                     dp[i][j] = dp[i-1][j-1]
                 else:
-                    dp[i][j] = dp[i-1][j-1] + rc
-                dp[i][j] = min([dp[i][j], dp[i-1][j]+dc])
-                dp[i][j] = min(dp[i][j], dp[i][j-1]+ic)
-
+                    # 三种情况取最小
+                    # 情况1：word1:xxxxE word2:xxxxF 替换尾部字符 说明等于前面字符的转换次数 + word1的1次替换代价
+                    # 情况2：word1:xxxD  word2:xxx   删除word1的尾部字符  说明等于前面字符的转换次数 + word1的1次删除代价
+                    # 情况3：word1:xxx  word2:xxxD   word1尾部添加字符   说明等于前面字符的转换次数 + word1的1次添加代价
+                    dp[i][j] = min(dp[i-1][j-1] + rc, dp[i-1][j] + dc, dp[i][j-1] + ic)
         return dp[len1][len2]
 
     # 空间压缩后
