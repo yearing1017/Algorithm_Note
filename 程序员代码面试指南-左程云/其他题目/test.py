80b29bbd372806454de765de63587e25968190d4
yearing1017@126.com <yearing1017@126.com>
2021-08-18 11:43:52 +0800

    update

--- 程序员代码面试指南-左程云/其他题目/test.py
+++ 程序员代码面试指南-左程云/其他题目/test.py
@@ -1,40 +1,27 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import xlrd
-import openpyxl
-import numpy as np
+'''
+给定一个 包含括号的字符串  计算 有效括号数 和 最大嵌套深度
+'''
+kh_str = '(()(()))'
 
-df = pd.read_excel('/Users/yearing1017/IdeaProjects/jupyter/job_key_status.xlsx', engine='openpyxl')
-group_result = df.loc[df.click != 0].loc[:,['push_date', 'expName', 'delivered', 'click']]
-# 给group_result计算一遍CTR
-group_result['CTR'] = (group_result['click'] / group_result['delivered']) * 100
+stack = []
+valid_num = 0
+max_depth = 0
+cur_depth = 0
 
-# 取最近7天
-date = group_result['push_date'].unique()[-7:]
-# 取最近7天的实验
-all_7_exp = set()
-for d in date:
-    group_result_1 = group_result.loc[group_result['push_date'] == d]
-    for exp in list(group_result_1['expName'].unique()):
-        all_7_exp.add(exp)
-all_7_exp = list(all_7_exp)
-# print(all_7_exp)
-# print(len(all_7_exp))
-# print(group_result)
-exp_ctr_dic = {}
-for exp in all_7_exp:
-    exp_ctr_dic[exp] = []
-#print(exp_ctr_dic)
-for i in range(len(date)):
-    temp_df = group_result[group_result['push_date'].isin([(date[i])])]
-    for j in range(len(temp_df)):
-        exp_ctr_dic[str(temp_df.iloc[j]['expName'])].append(temp_df.iloc[j]['CTR'])
-    #判断是否需要补0
-    for k in range(len(all_7_exp)):
-        if len(exp_ctr_dic[str(all_7_exp[k])]) == i+1:
+for s in kh_str:
+    if s == '(':
+        max_depth = max(max_depth, cur_depth)
+        cur_depth = 0
+        stack.append(s)
+    if s == ')':
+        if not stack:
             continue
         else:
-            exp_ctr_dic[str(all_7_exp[k])].append(0)
-for exp in all_7_exp:
-    print(exp)
-    print(exp_ctr_dic[0:2])
+            stack.pop()
+            valid_num += 1
+            cur_depth += 1
+# 应对 '(())((()))'
+max_depth = max(max_depth, cur_depth)
+
+print(valid_num)
+print(max_depth)
