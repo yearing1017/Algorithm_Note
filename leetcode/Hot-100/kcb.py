39941177daf8f60ef8254a170f04575043b77d45
yearing1017@126.com <yearing1017@126.com>
2022-03-07 16:27:04 +0800

    update

--- leetcode/Hot-100/kcb.py
+++ leetcode/Hot-100/kcb.py
@@ -42,3 +42,26 @@ class Solution:
         return not numCourses
 
 
+    '''
+    # 深度优先搜索  O(M+N) O(M+N) 节点数量和临边数量；
+    # flag = 0: 未被访问
+    # flag = -1: 被其他结点的DFS访问了
+    # flag = 1:  被自身结点的DFS访问了
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        def dfs(i, adjacency, flags):
+            if flags[i] == -1: return True
+            if flags[i] == 1: return False
+            flags[i] = 1
+            for j in adjacency[i]:
+                if not dfs(j, adjacency, flags): return False
+            flags[i] = -1
+            return True
+
+        adjacency = [[] for _ in range(numCourses)]
+        flags = [0 for _ in range(numCourses)]
+        for cur, pre in prerequisites:
+            adjacency[pre].append(cur)
+        for i in range(numCourses):
+            if not dfs(i, adjacency, flags): return False
+        return True
+    '''
