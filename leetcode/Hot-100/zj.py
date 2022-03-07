fa0ee3e33676ff5e8264ff1417cd836c1a332098
yearing1017@126.com <yearing1017@126.com>
2022-03-07 16:24:10 +0800

    update

--- leetcode/Hot-100/zj.py
+++ leetcode/Hot-100/zj.py
@@ -12,12 +12,13 @@ class Solution1:
         n = len(nums)
         def helper(i, temp):
             res.append(temp)
+            print(i, temp, res)
             for j in range(i, n):
                 # 每次都是将当前的temp去和他后面的元素组合
                 helper(j+1, temp + [nums[j]])
         helper(0, [])
         return res
-
+'''
 """
 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
@@ -37,7 +38,7 @@ class Solution:
                 helper(j+1, temp + [nums[j]])
         helper(0, [])
         return res
-
+'''
 if __name__ == "__main__":
     s = Solution1()
     nums = [1,2,3]
